ff5ca0b143ddf75077d01c9db98dfdbdbf3e4257
Caitlin Keck <caitlin.keck@gmail.com>
2017-09-08 21:57:27 -0400

    fixed up restaurants controller and edited route

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,9 @@
 class RestaurantsController < ApplicationController
 
-	def find_restaurant
+	def find_restaurants
 		query_term = params[:restaurant][:searchTerm]
 		response = HTTParty.get("https://api.foursquare.com/v2/venues/search?near=%27New%20York,%20NY%27&q=#{query_term}&categoryId=4d4b7105d754a06374d81259&client_id=RE3APGKQZOA2FCGSCF51X02RD10LJRU11JY2KD4R2VQWCXIU&client_secret=BH2GEJ4RZODQF3RLVZCMXQQWZB2Q4CKMRXTZJZUB00KPDPA0&v=20170908", format: :plain)
 		json = JSON.parse(response, {symbolize_names: true})
-		byebug
 		venues = json[:response][:venues]
 		render :json => {venues: venues}
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/recipes', to: 'recipes#find_recipe'
-  post '/restaurants', to: 'restaurants#find_restaurant'
+  post '/restaurants', to: 'restaurants#find_restaurants'
 end
